a2fd6d12b7d59800b862cd7b6890fec853e1a747
Joakim Hellsén <tlovinator@gmail.com>
2022-04-09 04:28:20 +0200

    Move while True: to main()

--- discord_free_game_notifier/main.py
+++ discord_free_game_notifier/main.py
@@ -2,6 +2,8 @@
 
 This file contains the main function for discord_free_game_notifier.
 """
+import time
+
 from dhooks import Webhook
 
 from discord_free_game_notifier import settings
@@ -16,23 +18,29 @@ def main():
     """Check for free games on Epic and Steam and send them to Discord.
     If there is an error, it will be sent to Discord.
     """
-    try:
-        epic_embed = get_free_epic_games()
-        for game in epic_embed:
-            hook.send(embed=game)
-        steam_embed = get_free_steam_games()
-        for game in steam_embed:
-            hook.send(embed=game)
-        gog_embed = get_free_gog_games()
-        for game in gog_embed:
-            hook.send(embed=game)
-    except Exception as exception:
-        hook.send(
-            f"Error: {exception}\nYou should write an issue at"
-            " https://github.com/TheLovinator1/discord-free-game-notifier/"
-            " or contact TheLovinator#9276 if this keeps happening :-)"
-        )
-        print(exception)
+    while True:
+        # Check for free games
+        try:
+            epic_embed = get_free_epic_games()
+            for game in epic_embed:
+                hook.send(embed=game)
+            steam_embed = get_free_steam_games()
+            for game in steam_embed:
+                hook.send(embed=game)
+            gog_embed = get_free_gog_games()
+            for game in gog_embed:
+                hook.send(embed=game)
+        except Exception as exception:
+            hook.send(
+                f"Error: {exception}\nYou should write an issue at"
+                " https://github.com/TheLovinator1/discord-free-game-notifier/"
+                " or contact TheLovinator#9276 if this keeps happening :-)"
+            )
+            print(exception)
+
+        # Sleep for 30 minutes
+        settings.logger.info("Sleeping for 30 minutes")
+        time.sleep(30 * 60)
 
 
 if __name__ == "__main__":
