c066b4a2cc8be172f333f152094b91fb501b436f
Joakim Hellsén <tlovinator@gmail.com>
2023-05-22 05:49:54 +0200

    Only grab the config settings if the config file is there

--- discord_free_game_notifier/settings.py
+++ discord_free_game_notifier/settings.py
@@ -1,36 +1,47 @@
 import configparser
 import os
+import sys
 from pathlib import Path
 
 from dotenv import load_dotenv
 from loguru import logger
 from platformdirs import user_data_dir
 
-load_dotenv()
-app_dir: str = user_data_dir(
-    "discord_free_game_notifier",
-    "TheLovinator",
-    roaming=True,
-)
+# Load environment variables from .env file
+load_dotenv(verbose=True)
+
+# Get our directory where we can store our data. This directory will contain the previous games we've posted.
+app_dir: str = user_data_dir("discord_free_game_notifier", "TheLovinator", roaming=True)
 Path.mkdir(Path(app_dir), exist_ok=True)
 
+# Get the config file location, this was used before environment variables were preferred.
 config_location: Path = Path(app_dir) / "config.conf"
 default_webhook_url: str = (
     "https://discord.com/api/webhooks/1234/567890/ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz"
 )
 
-# Read the config file
-config = configparser.ConfigParser()
-config.read(config_location)
+config_webhook_url = ""
+config_log_level = "INFO"
 
-# Get the webhook url from the config file
-config_webhook_url: str = config.get("config", "webhook_url")
+# Check if config file exists
+if config_location.exists():
+    # Read the config file
+    config = configparser.ConfigParser()
+    config.read(config_location)
 
-# Log severity. Can be CRITICAL, ERROR, WARNING, INFO or DEBUG.
-config_log_level: str = config.get("config", "log_level")
+    # Get the webhook url from the config file
+    config_webhook_url: str = config.get("config", "webhook_url")
+
+    # Log severity. Can be CRITICAL, ERROR, WARNING, INFO or DEBUG.
+    config_log_level: str = config.get("config", "log_level")
 
 # If user has environment variable set, use that instead
 webhook_url: str = os.getenv("WEBHOOK_URL", config_webhook_url)
+
+if not webhook_url:
+    logger.critical("No webhook url set. Please set the WEBHOOK_URL environment variable.")
+    sys.exit(1)
+
 log_level: str = os.getenv("LOG_LEVEL", config_log_level)
 
 steam_icon: str = os.getenv("STEAM_ICON", "https://lovinator.space/Steam_logo.png")
